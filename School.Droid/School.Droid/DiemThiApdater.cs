c37b3e271b22ad91c24a560e2002247a5d67daeb
txthao <thaotran261@gmail.com>
2015-10-19 22:52:04 +0700

    Update DT

--- School.Droid/School.Droid/DiemThiApdater.cs
+++ School.Droid/School.Droid/DiemThiApdater.cs
@@ -11,6 +11,7 @@ using Android.Widget;
 using School.Core;
 using Android.Graphics;
 using Java.Util;
+using Android.Graphics.Drawables;
 
 namespace School.Droid
 {
@@ -91,10 +92,20 @@ namespace School.Droid
 				case BODY_TYPE:
 					row = LayoutInflater.From (context).Inflate (Resource.Layout.DTRow, null);
 					viewholder.txtMonHocDT = row.FindViewById<TextView> (Resource.Id.txtMonHocDT);
+					viewholder.txtTiLe = row.FindViewById<TextView> (Resource.Id.txtTiLe);
+					viewholder.txtDT = row.FindViewById<TextView> (Resource.Id.txtDT);
+					viewholder.txtDTK = row.FindViewById<TextView> (Resource.Id.txtDTK);
+					viewholder.txtDKT = row.FindViewById<TextView> (Resource.Id.txtDKT);
+					viewholder.linear = row.FindViewById<LinearLayout> (Resource.Id.linear45);
 					break;
 				case FOOTER_TYPE:
 					row = LayoutInflater.From (context).Inflate (Resource.Layout.DTFooter, null);
 					viewholder.txtTB10 = row.FindViewById<TextView> (Resource.Id.txtTB10);
+					viewholder.txtTB4 = row.FindViewById<TextView> (Resource.Id.txtTB4);
+					viewholder.txtTBTL10 = row.FindViewById<TextView> (Resource.Id.txtTBTL10);
+					viewholder.txtTBTL4 = row.FindViewById<TextView> (Resource.Id.txtTBTL4);
+					viewholder.txtDRL = row.FindViewById<TextView> (Resource.Id.txtDRL);
+				
 					break;
 				}
 				row.Tag = viewholder;
@@ -111,19 +122,18 @@ namespace School.Droid
 			case BODY_TYPE:
 				MonHoc mh = BMonHoc.GetMH (SQLite_Android.GetConnection (), list [position].MaMH);
 				viewholder.txtMonHocDT.Text = mh.TenMH;
-//				row.FindViewById<TextView> (Resource.Id.txtTiLe).Text = Common.calPercent (mh.TiLeThi);
-//				row.FindViewById<TextView> (Resource.Id.txtDKT).Text = Common.checkSpaceValue (list [position].DiemKT);
-//				row.FindViewById<TextView> (Resource.Id.txtDT).Text = Common.checkSpaceValue (list [position].DiemThi);
-//				row.FindViewById<TextView> (Resource.Id.txtDTK).Text = Common.setDiemTK (list [position].DiemTK10, list [position].DiemChu);
-			
+				viewholder.txtTiLe.Text = Common.calPercent (mh.TiLeThi);
+				viewholder.txtDKT.Text = Common.checkSpaceValue (list [position].DiemKT);
+				viewholder.txtDT.Text = Common.checkSpaceValue (list [position].DiemThi);
+				viewholder.txtDTK.Text = Common.setDiemTK (list [position].DiemTK10, list [position].DiemChu);
 				break;
 			case FOOTER_TYPE:				
 				DiemThi dt = BDiemThi.GetDT (SQLite_Android.GetConnection (), list [position].Hocky, list [position].NamHoc);
 				viewholder.txtTB10.Text = "ĐTB Học kỳ hệ 10: " + dt.DiemTB10;
-//				row.FindViewById<TextView> (Resource.Id.txtTB4).Text = "ĐTB Học kỳ hệ 4: " + dt.DiemTB4;
-//				row.FindViewById<TextView> (Resource.Id.txtTBTL10).Text = "ĐTB Tích lũy hệ 10: " + dt.DiemTBTL10;
-//				row.FindViewById<TextView> (Resource.Id.txtTBTL4).Text = "ĐTB Tích lũy hệ 4: " + dt.DiemTBTL4;
-//				row.FindViewById<TextView> (Resource.Id.txtDRL).Text = "Điểm rèn luyện: " + dt.DiemRL;
+				viewholder.txtTB4.Text = "ĐTB Học kỳ hệ 4: " + dt.DiemTB4;
+				viewholder.txtTBTL10.Text = "ĐTB Tích lũy hệ 10: " + dt.DiemTBTL10;
+				viewholder.txtTBTL4.Text = "ĐTB Tích lũy hệ 4: " + dt.DiemTBTL4;
+				viewholder.txtDRL.Text = "Điểm rèn luyện: " + dt.DiemRL;
 				break;
 			}
 			return row;
@@ -132,39 +142,22 @@ namespace School.Droid
 		class ViewHolderItem : Java.Lang.Object
 		{
 			internal TextView txtTB10;
+			internal TextView txtTB4;
+			internal TextView txtTBTL10;
+			internal TextView txtTBTL4;
+			internal TextView txtDRL;
+
 			internal TextView txtMonHocDT;
+			internal TextView txtTiLe;
+			internal TextView txtDKT;
+			internal TextView txtDT;
+			internal TextView txtDTK;
+
 			internal TextView txtHeaderDT;
+
+			internal LinearLayout linear;
 		}
 
-//		public View setData(View row, int type, int position){
-//			switch (type) {
-//			case HEADER_TYPE:
-//				TextView title = row.FindViewById<TextView> (Resource.Id.txtHeaderDT);
-//				title.Text = " HỌC KỲ " + list [position].Hocky + " NĂM HỌC " + list [position].NamHoc;
-//				break;
-//			case BODY_TYPE:
-//				MonHoc mh = BMonHoc.GetMH (SQLite_Android.GetConnection (), list [position].MaMH);
-//				row.FindViewById<TextView> (Resource.Id.txtMonHocDT).Text = mh.TenMH;
-//				row.FindViewById<TextView> (Resource.Id.txtTiLe).Text = Common.calPercent (mh.TiLeThi);
-//				row.FindViewById<TextView> (Resource.Id.txtDKT).Text = Common.checkSpaceValue (list [position].DiemKT);
-//				row.FindViewById<TextView> (Resource.Id.txtDT).Text = Common.checkSpaceValue (list [position].DiemThi);
-//				row.FindViewById<TextView> (Resource.Id.txtDTK).Text = Common.setDiemTK (list [position].DiemTK10, list [position].DiemChu);
-//				if (position % 2 == 0) {
-//					row.FindViewById<LinearLayout> (Resource.Id.linear45).SetBackgroundColor (Color.ParseColor ("#FFFFFF"));
-//				} else {
-//					row.FindViewById<LinearLayout> (Resource.Id.linear45).SetBackgroundColor (Color.ParseColor ("#C9C9C9"));
-//				}
-//				break;
-//			case FOOTER_TYPE:
-//				DiemThi dt = BDiemThi.GetDT (SQLite_Android.GetConnection (), list [position].Hocky, list [position].NamHoc);
-//				row.FindViewById<TextView> (Resource.Id.txtTB10).Text = "ĐTB Học kỳ hệ 10: " + dt.DiemTB10;
-//				row.FindViewById<TextView> (Resource.Id.txtTB4).Text = "ĐTB Học kỳ hệ 4: " + dt.DiemTB4;
-//				row.FindViewById<TextView> (Resource.Id.txtTBTL10).Text = "ĐTB Tích lũy hệ 10: " + dt.DiemTBTL10;
-//				row.FindViewById<TextView> (Resource.Id.txtTBTL4).Text = "ĐTB Tích lũy hệ 4: " + dt.DiemTBTL4;
-//				row.FindViewById<TextView> (Resource.Id.txtDRL).Text = "Điểm rèn luyện: " + dt.DiemRL;
-//				break;
-//			}
-//		}
 //
 
 //		public List<DiemThi> listDT;
